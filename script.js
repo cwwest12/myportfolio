832690ae0499ab6a84244fb7503f76c81312dbc2
cwwest12 <114937058+cwwest12@users.noreply.github.com>
2023-07-24 15:29:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 const riddleContainer = document.getElementById("riddleBox");
+
+
+const riddles = ['What comes down but never goes up?', 'I am not alive, but I grow; I don\'t have lungs, but I need air; I don\'t have a mouth, but water kills me. What am I?', 'What has a heart that doesn\'t beat, and leaves but never walks?', 'I am always in front of you but can never be seen. What am I?', 'What gets bigger when more is taken away from it?']
